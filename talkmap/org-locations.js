b3611ff91c40aca50039d1c563484d4c502d28e9
Katherine Furman <57194837+katiefurms@users.noreply.github.com>
2019-11-22 11:36:25 -0500

    revert org-locations back to original

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,32 +1,22 @@
 var addressPoints = [
   [
-    "New York, NY",
-    40.73023,
-    -73.99569
+    "Berkeley CA, USA",
+    37.8708393,
+    -122.2728638
   ],
   [
-    "Washington D.C.",
-    38.904884,
-    -77.022993
+    "London, UK",
+    51.5073219,
+    -0.1276473
   ],
   [
-    "Indianapolis, IN",
-    39.764854,
-    -86.165321
+    "San Francisco, California",
+    37.7792808,
+    -122.4192362
   ],
   [
-    "New Haven, CT",
-    41.3032,
-    -72.9339
-  ],
-  [
-    "Ann Arbor, MI",
-    42.2833,
-    -83.7279
-  ],
-  [
-    "San Diego, CA",
-    32.7070,
-    -117.1630
+    "Los Angeles, CA",
+    34.0543942,
+    -118.2439408
   ]
-];
+]; 
